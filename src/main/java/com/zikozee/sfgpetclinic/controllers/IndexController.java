38f41822238ad956e012d5b588c4cf93200ce1b5
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-04-16 00:30:05 +0100

    first commit

---
+++ src/main/java/com/zikozee/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.zikozee.sfgpetclinic.controllers;
+
+public class IndexController {
+
+    public String index(){
+
+        return "index";
+    }
+
+    public String oupsHandler(){
+        return "notimplemented";
+    }
+}
