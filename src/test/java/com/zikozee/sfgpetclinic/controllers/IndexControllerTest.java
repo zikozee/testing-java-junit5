c74ff8299854fc7e347b8b217f15df9dc79b6228
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-04-17 19:39:00 +0100

    Using AssertJ with Junit

--- src/test/java/com/zikozee/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/com/zikozee/sfgpetclinic/controllers/IndexControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
@@ -28,6 +29,10 @@ class IndexControllerTest {
 
         assertEquals("index", controller.index(), () -> "Another Expensive Message " +    //lambda only runs in failure conditions
                 "Make me only if you have to");
+
+        //ASSERTJ
+        assertThat(controller.index()).isEqualTo("index");
+        assertThat(controller.index()).contains("ex");
     }
 
     @Test
--- src/test/java/com/zikozee/sfgpetclinic/model/PersonTest.java
+++ src/test/java/com/zikozee/sfgpetclinic/model/PersonTest.java
@@ -1,7 +1,12 @@
 package com.zikozee.sfgpetclinic.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
@@ -26,5 +31,20 @@ class PersonTest {
         assertAll("Test Prop Set",
                 () -> assertEquals("Joe", person.getFirstName(), "First Name Failed"),
                 () -> assertEquals("Buck", person.getLastName(),"Last Name Failed"));
+    }
+
+    @DisplayName(value = "using assertJ with Junit")
+    @Test
+    void collectionTestWithAssertJ() {
+        Person person1 = new Person(1L, "Ziko", "Zee");
+        Person person2 = new Person(2L, "Bobby", "Ray");
+        Person person3 = new Person(3L, "Bambu", "Bee");
+        List<Person> persons = Arrays.asList(person1, person2);
+
+        assertThat(person2).isIn(persons);
+        assertThat(person3).isNotIn(persons);
+
+        assertThat(person1).matches(person -> person.getFirstName().equals("Ziko") && person.getLastName().equals("Zee"));
+        assertThat(persons).isNotEmpty();
     }
 }
